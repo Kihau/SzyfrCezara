4fad4a2d213fcee28787b35c3eb531a852ee337f
Michał Hałdaś <michal.haldas@gmail.com>
2020-04-07 22:15:44 +0200

    Update

---
+++ Program/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonZaszyfruj = new System.Windows.Forms.Button();
+            this.checkBoxSpacja = new System.Windows.Forms.CheckBox();
+            this.labelZnaki = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxWejście = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxWyjście = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxKlucz = new System.Windows.Forms.TextBox();
+            this.buttonOdszyfruj = new System.Windows.Forms.Button();
+            this.checkBoxPolski = new System.Windows.Forms.CheckBox();
+            this.textBoxCiąg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonZaszyfruj
+            // 
+            this.buttonZaszyfruj.Location = new System.Drawing.Point(34, 164);
+            this.buttonZaszyfruj.Name = "buttonZaszyfruj";
+            this.buttonZaszyfruj.Size = new System.Drawing.Size(82, 73);
+            this.buttonZaszyfruj.TabIndex = 1;
+            this.buttonZaszyfruj.Text = "ZASZYFRUJ";
+            this.buttonZaszyfruj.UseVisualStyleBackColor = true;
+            this.buttonZaszyfruj.Click += new System.EventHandler(this.buttonZaszyfruj_Click);
+            // 
+            // checkBoxSpacja
+            // 
+            this.checkBoxSpacja.AutoSize = true;
+            this.checkBoxSpacja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.checkBoxSpacja.Location = new System.Drawing.Point(362, 154);
+            this.checkBoxSpacja.Name = "checkBoxSpacja";
+            this.checkBoxSpacja.Size = new System.Drawing.Size(109, 44);
+            this.checkBoxSpacja.TabIndex = 2;
+            this.checkBoxSpacja.Text = "Uwzględnij\r\nspację\r\n";
+            this.checkBoxSpacja.UseVisualStyleBackColor = true;
+            this.checkBoxSpacja.CheckedChanged += new System.EventHandler(this.checkBoxSpacja_CheckedChanged);
+            // 
+            // labelZnaki
+            // 
+            this.labelZnaki.AutoSize = true;
+            this.labelZnaki.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.labelZnaki.Location = new System.Drawing.Point(114, 9);
+            this.labelZnaki.Name = "labelZnaki";
+            this.labelZnaki.Size = new System.Drawing.Size(254, 26);
+            this.labelZnaki.TabIndex = 3;
+            this.labelZnaki.Text = "Ciąg dostępnych znaków";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(114, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tu wprowadź wiadomość:";
+            // 
+            // textBoxWejście
+            // 
+            this.textBoxWejście.Location = new System.Drawing.Point(12, 128);
+            this.textBoxWejście.Name = "textBoxWejście";
+            this.textBoxWejście.Size = new System.Drawing.Size(459, 20);
+            this.textBoxWejście.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.Location = new System.Drawing.Point(7, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(470, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Twoja zaszyfrowana/odszyfrowana wiadomość:";
+            // 
+            // textBoxWyjście
+            // 
+            this.textBoxWyjście.Location = new System.Drawing.Point(12, 277);
+            this.textBoxWyjście.Name = "textBoxWyjście";
+            this.textBoxWyjście.Size = new System.Drawing.Size(459, 20);
+            this.textBoxWyjście.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label3.Location = new System.Drawing.Point(267, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Klucz:";
+            // 
+            // textBoxKlucz
+            // 
+            this.textBoxKlucz.Location = new System.Drawing.Point(263, 193);
+            this.textBoxKlucz.Name = "textBoxKlucz";
+            this.textBoxKlucz.Size = new System.Drawing.Size(76, 20);
+            this.textBoxKlucz.TabIndex = 9;
+            // 
+            // buttonOdszyfruj
+            // 
+            this.buttonOdszyfruj.Location = new System.Drawing.Point(144, 164);
+            this.buttonOdszyfruj.Name = "buttonOdszyfruj";
+            this.buttonOdszyfruj.Size = new System.Drawing.Size(84, 73);
+            this.buttonOdszyfruj.TabIndex = 10;
+            this.buttonOdszyfruj.Text = "ODSZYFRUJ";
+            this.buttonOdszyfruj.UseVisualStyleBackColor = true;
+            this.buttonOdszyfruj.Click += new System.EventHandler(this.buttonOdszyfruj_Click);
+            // 
+            // checkBoxPolski
+            // 
+            this.checkBoxPolski.AutoSize = true;
+            this.checkBoxPolski.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.checkBoxPolski.Location = new System.Drawing.Point(362, 204);
+            this.checkBoxPolski.Name = "checkBoxPolski";
+            this.checkBoxPolski.Size = new System.Drawing.Size(109, 44);
+            this.checkBoxPolski.TabIndex = 11;
+            this.checkBoxPolski.Text = "Uwzględnij\r\npolskie\r\n";
+            this.checkBoxPolski.UseVisualStyleBackColor = true;
+            this.checkBoxPolski.CheckedChanged += new System.EventHandler(this.checkBoxPolski_CheckedChanged);
+            // 
+            // textBoxCiąg
+            // 
+            this.textBoxCiąg.Location = new System.Drawing.Point(110, 38);
+            this.textBoxCiąg.Multiline = true;
+            this.textBoxCiąg.Name = "textBoxCiąg";
+            this.textBoxCiąg.ReadOnly = true;
+            this.textBoxCiąg.Size = new System.Drawing.Size(258, 46);
+            this.textBoxCiąg.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 318);
+            this.Controls.Add(this.textBoxCiąg);
+            this.Controls.Add(this.checkBoxPolski);
+            this.Controls.Add(this.buttonOdszyfruj);
+            this.Controls.Add(this.textBoxKlucz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxWyjście);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxWejście);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelZnaki);
+            this.Controls.Add(this.checkBoxSpacja);
+            this.Controls.Add(this.buttonZaszyfruj);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Szyfr Cezara - Michał Hałdaś";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonZaszyfruj;
+        private System.Windows.Forms.CheckBox checkBoxSpacja;
+        private System.Windows.Forms.Label labelZnaki;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxWejście;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxWyjście;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxKlucz;
+        private System.Windows.Forms.Button buttonOdszyfruj;
+        private System.Windows.Forms.CheckBox checkBoxPolski;
+        private System.Windows.Forms.TextBox textBoxCiąg;
+    }
+}
+
---
+++ Program/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ZaładujZnaki();
+            textBoxCiąg.Text = AktualizujZnaki();
+            textBoxCiąg.ScrollBars = ScrollBars.Vertical;
+        }
+
+        List<char> znaki = new List<char>();
+        List<char> polskie = new List<char> { 'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'Ą', 'Ć', 'Ę', 'Ł', 'Ń', 'Ó', 'Ś', 'Ź', 'Ż' };
+        bool JestSpacja { get; set; }
+        bool SąPolskie { get; set; }
+
+        private void ZaładujZnaki()
+        {
+            for (int i = 97; i <= 122; i++)
+                znaki.Add((char)i);
+
+            for (int i = 65; i <= 90; i++)
+                znaki.Add((char)i);
+        }
+
+        private string AktualizujZnaki()
+        {
+            string napis = "";
+
+            foreach (char c in znaki)
+                if (c == ' ') napis += "spacja, ";
+                else napis += c.ToString() + ", ";
+            
+            return napis;
+        }
+
+        private void buttonZaszyfruj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder builder = new StringBuilder(textBoxWejście.Text);
+                int klucz = Int32.Parse(textBoxKlucz.Text);
+
+                for (int i = 0; i < builder.Length && klucz < znaki.Count; i++)
+                {
+                    int nowyindex = znaki.IndexOf(builder[i]) + klucz;
+                    if (nowyindex >= znaki.Count) builder[i] = znaki[nowyindex - znaki.Count];
+                    else builder[i] = znaki[nowyindex];
+                }
+
+                textBoxWyjście.Text = builder.ToString();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Wprowadzono niepoprawne dane!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonOdszyfruj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder builder = new StringBuilder(textBoxWejście.Text);
+                int klucz = Int32.Parse(textBoxKlucz.Text);
+
+                for (int i = 0; i < builder.Length && klucz < znaki.Count; i++)
+                {
+                    int nowyindex = znaki.IndexOf(builder[i]) - klucz;
+                    if (nowyindex < 0) builder[i] = znaki[nowyindex + znaki.Count];
+                    else builder[i] = znaki[nowyindex];
+                }
+
+                textBoxWyjście.Text = builder.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wprowadzono niepoprawne dane!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBoxSpacja_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!JestSpacja) znaki.Add(' ');
+            else znaki.Remove(' ');
+            textBoxCiąg.Text = AktualizujZnaki();
+            JestSpacja = !JestSpacja;
+        }
+
+        private void checkBoxPolski_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!SąPolskie) znaki.AddRange(polskie);
+            else znaki.RemoveRange(znaki.IndexOf(polskie[0]), polskie.Count);
+            textBoxCiąg.Text = AktualizujZnaki();
+            SąPolskie = !SąPolskie;
+        }
+    }
+}
